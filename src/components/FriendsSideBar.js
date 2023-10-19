e68de402f74790292236791cd13dd324e1f8e884
MrRomaLol <romaro422@gmail.com>
2023-10-19 19:35:35 +0300

    More updates

--- src/components/FriendsSideBar.js
+++ src/components/FriendsSideBar.js
@@ -18,11 +18,28 @@ const BarBorder = styled(BarsBorder)`
 `
 
 const Bar = styled(LRBars)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
   clip-path: polygon(0 0, calc(100% - 20px) 0, 100% 30px, 100% calc(100% - 30px), calc(100% - 20px) 100%, 0 100%);
 `
 
+const Scroller = styled.div`
+  overflow-y: scroll;
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
 const FriendsSideBar = () => {
-    
+
 
     return (
         <EobaniyBlyr>
@@ -30,6 +47,33 @@ const FriendsSideBar = () => {
                 <BarButton icon={faUser}/>
                 <BarButton icon={faPlus}/>
                 <IconSeparator/>
+
+                <div style={{flex: 1, position: 'relative'}}>
+                    <Scroller>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+                        <BarButton icon={faPlus}/>
+
+                    </Scroller>
+                </div>
+
             </Bar>
             <BarBorder/>
         </EobaniyBlyr>
